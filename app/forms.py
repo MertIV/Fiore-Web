b867580b63a65e700137555517f3cc4d959d589e
MertIV <mrtkrt96@gmail.com>
2022-05-20 18:05:17 +0300

    feat: male transplant font correction

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms            import StringField, TextAreaField, SubmitField, Password
 from wtforms.validators import InputRequired, Email, DataRequired
 
 class ContactForm(FlaskForm):
-	first_name   = StringField  (_l('First Name')     , validators=[] )
-	last_name    = StringField  (_l('Last Name')  , validators=[])
-	phone	     = StringField  (_l('Phone Number or E-mail')  , validators=[DataRequired()])
-	message      = StringField  (_l('Message')     , validators=[])
+	first_name   = StringField  (_l('First Name')     			, validators=[])
+	last_name    = StringField  (_l('Last Name')  				, validators=[])
+	phone	     = StringField  (_l('Phone Number or E-mail')  	, validators=[DataRequired()])
+	message      = StringField  (_l('Message')     				, validators=[])
